9c0b30b1f7291c0be26d3e67a51b415935e0d425
Igor <acenkoigor44@gmail.com>
2025-05-30 16:16:38 +0300

    Deploy-finally-set-fix-bags8

--- app/adminPage/page.js
+++ app/adminPage/page.js
@@ -2,6 +2,7 @@ import { prisma } from '../lib/prisma';
 import AdminPageClient from '../components/adminPageClient';
 import CategoryManager from '../components/CategoryManager';
 
+export const dynamic = 'force-dynamic';
 
 export default async function AdminPage() {
   const categories = await prisma.category.findMany({
